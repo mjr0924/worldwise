139eae68cb4b4f4ae0b2a962d599767394d36174
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-03-22 11:56:22 +0330

    Add cityList Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,52 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayOut";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes >
-      <Route path="/" element={<HomePage />} />
-      <Route path="product" element={<Product />} />
-      <Route path="pricing" element={<Pricing />} />
-      <Route path="login" element={<Login />} />
-      <Route path="app" element={<AppLayout />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  </BrowserRouter>;
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data... ");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import APPNav from "./APPNav";
+import { Outlet } from "react-router-dom";
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <APPNav />
 
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
