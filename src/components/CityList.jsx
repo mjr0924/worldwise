d1d6866be92a5475ea98a9565595de5a5a335651
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-04-10 23:33:23 +0330

    add contextAPI

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,7 +2,9 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
-export default function CityList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+export default function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city by a city on the map" />;
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,7 +2,9 @@ import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
-export default function CountryList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city by a city on the map" />;
